8d0505459cca62c3ab3129276b23ab3624d62173
Vitalie Safronovici <vitalie.safronovici@endava.com>
2018-08-07 10:24:47 +0300

    polishing 1

--- app/scripts/routes/FormDemoPage.jsx
+++ app/scripts/routes/FormDemoPage.jsx
@@ -55,7 +55,7 @@ export class FormDemoPage extends React.Component {
   }
 
   render() {
-    console.log('--', this.props)
+    console.log('----', this.props)
     return (
       <form>
         <Field name="firstName" component={RenderInput} placeholder="Street" />
