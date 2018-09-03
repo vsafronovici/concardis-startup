677a99826ebcc29b9bab0f65cb6e24716dcd181f
Stanislav Litvinov <unreal-3@bk.ru>
2018-09-03 17:24:05 +0300

    merge conflicts

--- app/scripts/components/MainFooter.jsx
+++ app/scripts/components/MainFooter.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
-<<<<<<< HEAD
 import { Slider } from 'antd';
-=======
->>>>>>> 587d497b8dc78fdde6847c5a407c4ceafd4579ff
 
 const { Footer } = Layout;
 
