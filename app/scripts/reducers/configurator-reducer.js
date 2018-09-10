78153339a8bcfb63b08987e9ea152b1c5ba79b14
Vitalie Safronovici <vitalie.safronovici@endava.com>
2018-09-10 11:58:57 +0300

    sprint1 added constants

--- app/scripts/reducers/configurator-reducer.js
+++ app/scripts/reducers/configurator-reducer.js
@@ -1,11 +1,11 @@
 import { createReducer } from './../modules/helpers'
 import { CONFIGURATOR } from './../actions/types'
-import { SectionStatusType } from '../utils/constants'
+import { ConfiguratorPageStep } from '../utils/constants'
 import { keys, pickBy, toPairs, isEmpty } from 'ramda'
 
 export const initialState = {
   fields: {},
-  step: 1,
+  step: ConfiguratorPageStep.STEP1,
   submitting: false,
   step1MetaData: undefined
 
