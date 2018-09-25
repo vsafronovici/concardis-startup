13ffc3fcbd232b91b2b5920c4033beb3e6b9551c
Stanislav Litvinov <stanislav.litvinov@endava.com>
2018-09-25 15:34:14 +0300

    Option card 2

--- app/scripts/components/configurator/OptionCard2.jsx
+++ app/scripts/components/configurator/OptionCard2.jsx
@@ -15,16 +15,44 @@ export class OptionCard extends React.Component {
 
   render() {
     const { } = this.props
-
+    const mock = ['Product feature 1', 'Product feature 2','Product feature 3','Product feature 4']
     return (
       <div className="option-card2">
-        <div className="oc-top" />
-        <div className="oc-content">
-          <div className="oc-title">name</div>
-          <div className="oc-subtitle">description</div>
-          <div className="oc-items">
+        <div className="oc-top">
+          <div className="oc-top-image">
+            <h2>image</h2>
           </div>
         </div>
+        <div className="oc-content">
+          <div className="oc-title">
+            Mobile package A
+          </div>
+          <div className="oc-subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud 
+          </div>
+          <div className="oc-items">
+          {mock.map((item, index) => {
+            return(
+              <div key={index} className="oc-item">
+              {item}
+              </div>
+            )
+          })}
+            </div>
+          </div>
+
+          <hr/>
+          <div className="oc-footer">
+            <div className="oc-footer-title">
+              Footer Title
+            </div>
+            <div>
+              Footer description
+            </div>
+          </div>
+        
       </div>
     )
   }
