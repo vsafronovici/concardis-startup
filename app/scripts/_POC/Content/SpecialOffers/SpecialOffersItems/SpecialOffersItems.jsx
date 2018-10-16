7f1b0f57e99593f38cf1d1faa4b6fdf9bb07955c
Vitalie Safronovici <vitalie.safronovici@endava.com>
2018-10-16 12:12:40 +0300

    removed POC

--- app/scripts/_POC/Content/SpecialOffers/SpecialOffersItems/SpecialOffersItems.jsx
+++
@@ -1,6 +0,0 @@
-
-export const itemsSpecialOffers = [{
-  title: 'Special Offers',
-  content: `Accept card payments online, offline or at the customer’s premises. 
-            In just two clicks with our offers only for you.`
-}]
--- app/scripts/mock-data/mock-fields-values.js
+++
@@ -1,8 +0,0 @@
-export const mockFieldsValues = {
-  1: {
-    f1: '3',
-    f2: 'Yes'
-  },
-  2: {}
-}
-
